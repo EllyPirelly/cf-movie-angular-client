798c2aa85ee860f89aa9c97679f7921129f2c4ce
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-06-02 13:16:40 +0200

    create log-in form, add logic, test styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -16,7 +17,13 @@ export class AppComponent {
 
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
+      width: '100%'
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '100%'
     });
   }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+  // defines the components input
+  @Input() userData = {
+    userName: '',
+    password: ''
+  };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    // modal, toggled at success/no success
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    // user notification
+    public snackBar: MatSnackBar) { }
+
+  // is called once the component has recieved all its inputs
+  ngOnInit(): void {
+  }
+  // sends form inputs to backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      // Logic for a successful user registration goes here! (To be implemented)
+
+      // closes modal on success
+      this.dialogRef.close();
+      console.log(response);
+
+      this.snackBar.open('user logged in successfully!', 'OK', {
+        duration: 6000
+      });
+
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
